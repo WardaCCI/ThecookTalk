bedb6b7f3c9d83bcc482cb3b71ced73053756214
Ndieme Cisse <ndiemecisse4@gmail.com>
2024-02-27 21:20:27 +0100

    ndieme1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+// Route pour afficher le formulaire d'inscription
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register.form');
+
+// Route pour traiter la soumission du formulaire d'inscription
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+
+// Route pour afficher le formulaire de connexion
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form');
+
+// Route pour traiter la soumission du formulaire de connexion
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+// Route pour déconnecter l'utilisateur
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
